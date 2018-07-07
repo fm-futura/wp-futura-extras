e9c3bc8fa1c6b919ddc3bf6fa92f939bcf3a3f78
Adrián Pardini <github@tangopardo.com.ar>
2018-07-07 10:43:34 -0300

    adds basic plugin skeleton

---
+++ wp-futura-shortcodes.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Plugin Name:        FM Futura Shortcodes
+Plugin URI:         https://github.com/fm-futura/wp-futura-shortcodes
+GitHub Plugin URI:  https://github.com/fm-futura/wp-futura-shortcodes
+Description:        Shortcodes para FM Futura
+Version:            20180707
+Author:             FM Futura
+Author URI:         https://fmfutura.com.ar
+License:            AGPL-3.0
+License URI:        https://www.gnu.org/licenses/agpl-3.0.html
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
