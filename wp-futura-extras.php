d523af51208e93e11994649741f4845c87864ae5
Adrián Pardini <github@tangopardo.com.ar>
2018-08-21 19:28:43 -0300

    bump version

--- wp-futura-extras.php
+++ wp-futura-extras.php
@@ -4,7 +4,7 @@ Plugin Name:        FM Futura Extras
 Plugin URI:         https://github.com/fm-futura/wp-futura-extras
 GitHub Plugin URI:  https://github.com/fm-futura/wp-futura-extras
 Description:        Shortcodes y otras cosas para FM Futura
-Version:            20180723
+Version:            20180821
 Author:             FM Futura
 Author URI:         https://fmfutura.com.ar
 License:            AGPL-3.0
