48dd074568f6039b279b0fd2e6139d8c2566e1df
Adrián Pardini <github@tangopardo.com.ar>
2020-06-02 20:28:59 -0300

    social icons: use text with our font instead of image, fix wrapping

--- shortcodes/futura_social_icons.php
+++ shortcodes/futura_social_icons.php
@@ -4,31 +4,36 @@ $shortcodes_base_url = plugins_url('shortcodes/', dirname(__FILE__));
 
 <div class="futura-social-icons" id="wp_futura_social_icons">
 
-        <div class="texto_radio_futura" > <img src="<?php echo $shortcodes_base_url ?>texto_radio_futura.svg" /> </div>
+        <span class="texto_radio_futura font-futura">
+            RADIO FUTURA
+        </span>
 
-        <span class="td-social-icon-wrap">
-            <a target="_blank" href="https://api.whatsapp.com/send?phone=5492216190382&amp;text=&amp;source=&amp;data=" title="WhatsApp">
-                <i class="td-icon-font td-icon-whatsapp"></i>
-            </a>
-        </span>
-        <span class="td-social-icon-wrap">
-            <a target="_blank" href="https://www.facebook.com/RadioFutura905/" title="Facebook">
-                <i class="td-icon-font td-icon-facebook"></i>
-            </a>
-        </span>
-        <span class="td-social-icon-wrap">
-            <a target="_blank" href="https://www.instagram.com/radiofuturafm90.5" title="Instagram">
-                <i class="td-icon-font td-icon-instagram"></i>
-            </a>
-        </span>
-        <span class="td-social-icon-wrap">
-            <a target="_blank" href="https://twitter.com/Futura905" title="Twitter">
-                <i class="td-icon-font td-icon-twitter"></i>
-            </a>
-        </span>
-        <span class="td-social-icon-wrap">
-            <a target="_blank" href="https://www.youtube.com/channel/UCLtoIsQ5C0P4kdJMwB45nRw" title="Youtube">
-                <i class="td-icon-font td-icon-youtube"></i>
-            </a>
-        </span>
+        <div class="futura-social-icons-container">
+            <span class="td-social-icon-wrap">
+                <a target="_blank" href="https://api.whatsapp.com/send?phone=5492216190382&amp;text=&amp;source=&amp;data=" title="WhatsApp">
+                    <i class="td-icon-font td-icon-whatsapp"></i>
+                </a>
+            </span>
+
+            <span class="td-social-icon-wrap">
+                <a target="_blank" href="https://www.facebook.com/RadioFutura905/" title="Facebook">
+                    <i class="td-icon-font td-icon-facebook"></i>
+                </a>
+            </span>
+            <span class="td-social-icon-wrap">
+                <a target="_blank" href="https://www.instagram.com/radiofuturafm90.5" title="Instagram">
+                    <i class="td-icon-font td-icon-instagram"></i>
+                </a>
+            </span>
+            <span class="td-social-icon-wrap">
+                <a target="_blank" href="https://twitter.com/Futura905" title="Twitter">
+                    <i class="td-icon-font td-icon-twitter"></i>
+                </a>
+            </span>
+            <span class="td-social-icon-wrap">
+                <a target="_blank" href="https://www.youtube.com/channel/UCLtoIsQ5C0P4kdJMwB45nRw" title="Youtube">
+                    <i class="td-icon-font td-icon-youtube"></i>
+                </a>
+            </span>
+    </div>
 </div>
