4b9acf569d5c1803ce060c46b8dd440d7f8061e1
Adrián Pardini <github@tangopardo.com.ar>
2020-06-02 18:55:34 -0300

    styles: load our font as Futura Radio Regular, define font-futura helper class

--- styles.php
+++ styles.php
@@ -5,5 +5,6 @@ function wp_futura_styles_init()
     wp_enqueue_style('futura_author_styles',        plugins_url('css/author.css', __FILE__));
     wp_enqueue_style('futura_social_icons_styles',  plugins_url('css/futura-social-icons.css', __FILE__));
     wp_enqueue_style('futura_google_fonts',         plugins_url('css/google-fonts.css', __FILE__));
+    wp_enqueue_style('futura_futura_fonts',         plugins_url('css/futura-fonts.css', __FILE__));
 }
 add_action('wp_enqueue_scripts', 'wp_futura_styles_init');
