e81f506ba4705c0096e189b6c039ff5e710876ab
Adrián Pardini <github@tangopardo.com.ar>
2020-09-13 18:15:31 -0300

    enable turbolinks based on admin setting

--- scripts.php
+++ scripts.php
@@ -3,7 +3,9 @@
 function wp_futura_scripts_init()
 {
     if (!is_user_logged_in()) {
-        wp_enqueue_script('futura_turbolinks',    plugins_url('libs/turbolinks/turbolinks.js',  __FILE__));
+        if (get_option('futura_enable_turbolinks')) {
+            wp_enqueue_script('futura_turbolinks',    plugins_url('libs/turbolinks/turbolinks.js',  __FILE__));
+        }
     }
 }
 add_action('wp_enqueue_scripts', 'wp_futura_scripts_init');
