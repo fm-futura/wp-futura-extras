e1995d507f1a1c3ebedc2da6f8c8cab692b66e8d
Adrián Pardini <github@tangopardo.com.ar>
2020-10-06 23:44:22 -0300

    futura-links: match to new design

--- shortcodes/futura_links.php
+++ shortcodes/futura_links.php
@@ -5,14 +5,14 @@ $img_base_url = plugins_url('img/', dirname(__FILE__));
 
 <div class="futura-links-farco-sir" id="futura-links-farco-sir">
     <div class="futura-links-container">
-        <div class="futura-link farco">
-            <a href="farco"  style="background-image: url('<?php echo $img_base_url; ?>logo_farco-256px.png');"> </a>
-        </div>
         <div class="futura-link sir">
-            <a href="sir"    style="background-image: url('<?php echo $img_base_url; ?>logo_sir-256px.png');"> </a>
+            <a href="sir"    style="background-image: url('<?php echo $img_base_url; ?>sir_con_flecha.png');"> </a>
         </div>
-        <div class="futura-link futura">
-            <a href="futura" style="background-image: url('<?php echo $img_base_url; ?>logo_futura-256px.png');"> </a>
+        <div class="futura-link agencia-farco">
+            <a href="futura" style="background-image: url('<?php echo $img_base_url; ?>producimos_en_red_agencia_farco.png');"> </a>
+        </div>
+        <div class="futura-link farco">
+            <a href="farco"  style="background-image: url('<?php echo $img_base_url; ?>somos_parte_farco.png');"> </a>
         </div>
     </div>
 </div>
