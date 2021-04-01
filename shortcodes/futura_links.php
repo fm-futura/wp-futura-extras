1cd1a92090fda32bee736dc659f6a09b38283f60
Adrián Pardini <github@tangopardo.com.ar>
2021-04-01 00:19:02 -0300

    futura-links: fix target urls, minor style changes

--- shortcodes/futura_links.php
+++ shortcodes/futura_links.php
@@ -6,13 +6,13 @@ $img_base_url = plugins_url('img/', dirname(__FILE__));
 <div class="futura-links-farco-sir" id="futura-links-farco-sir">
     <div class="futura-links-container">
         <div class="futura-link sir">
-            <a href="sir"    style="background-image: url('<?php echo $img_base_url; ?>sir_con_flecha.png');"> </a>
+            <a href="https://informativoregional.com.ar"    style="background-image: url('<?php echo $img_base_url; ?>sir_con_flecha.png');"> </a>
         </div>
         <div class="futura-link agencia-farco">
-            <a href="futura" style="background-image: url('<?php echo $img_base_url; ?>producimos_en_red_agencia_farco.png');"> </a>
+            <a href="https://agencia.farco.org.ar" style="background-image: url('<?php echo $img_base_url; ?>producimos_en_red_agencia_farco.png');"> </a>
         </div>
         <div class="futura-link farco">
-            <a href="farco"  style="background-image: url('<?php echo $img_base_url; ?>somos_parte_farco.png');"> </a>
+            <a href="https://farco.org.ar"  style="background-image: url('<?php echo $img_base_url; ?>somos_parte_farco.png');"> </a>
         </div>
     </div>
 </div>
