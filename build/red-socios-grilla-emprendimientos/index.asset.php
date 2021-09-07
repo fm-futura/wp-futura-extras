17fd9a22f202735a8bbea0382682013ac155729d
Adrián Pardini <github@tangopardo.com.ar>
2021-09-07 00:06:21 -0300

    blocks: update build

---
+++ build/red-socios-grilla-emprendimientos/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-polyfill', 'wp-server-side-render'), 'version' => '18e1498611ccb35c8f65501224bc2242');
---
+++ build/red-socios-metabox/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-components', 'wp-compose', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-plugins', 'wp-polyfill'), 'version' => '25224b7e643e9b64e6f6cf3276c1e7ad');
