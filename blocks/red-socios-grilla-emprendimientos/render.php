f2c0ea48a0025ba5edc628207710b1a10c1f8f3b
Adrián Pardini <github@tangopardo.com.ar>
2021-09-11 14:42:44 -0300

    red-socios-grilla-emprendimientos: cache results, use smaller thumbs

--- blocks/red-socios-grilla-emprendimientos/render.php
+++ blocks/red-socios-grilla-emprendimientos/render.php
@@ -6,12 +6,14 @@ use WP_Query;
 
 function render ($block_attributes, $content) {
     $query = new WP_Query([
+        'cache_results' => true,
         'post_type' => 'emprendimiento-red',
+        'posts_per_page' => -1,
     ]);
 
     $items = [];
     foreach ($query->posts as $post) {
-        $thumb = get_the_post_thumbnail($post, 'large');
+        $thumb = get_the_post_thumbnail($post, 'medium');
         $title = $post->post_title;
         $link = get_post_meta($post->ID, 'main_link', true);
 
